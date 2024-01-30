91e22fa7f5e7a4fa45030613f4e67dc475cf25c3
sendkeyz <158175978+sendkeyz@users.noreply.github.com>
2024-01-30 13:54:44 +0000

    Add files via upload

---
+++ sndKeyzgui.php
@@ -0,0 +1,16 @@
+<html>
+	
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<form action="sndKeyz.php" method="post">
+<input type="text" autocomplete="off" size="50" name="cpkey">
+<input type="submit">
+</form>
+
+
+
+</body>
+</html>
